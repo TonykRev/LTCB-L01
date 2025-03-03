78bb86cfef567c42c50b9375c1001b72f7c3682d
Git-Huy-cyber <tranhuydh81@gmail.com>
2025-03-03 05:02:17 -0500

    Add files via upload

---
+++ bai4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float R; 
+    const float pi=3.1415;
+    float S, V;
+    S=4 * pi * R * R;
+    V=(4.0 / 3) * pi * R * R * R;
+    scanf("nhập bán kính %.2f", &R);
+    printf("Dien tich cua hinh cau la: %.2f\n", S);
+    printf("The tich cua hinh cau la: %.2f\n", V);
+}
